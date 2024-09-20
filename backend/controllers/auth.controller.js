420fc7e390052fd33720b919fa53deb494d2f21c
MCDoodle1 <mclarijs@me.com>
2024-09-20 14:52:08 +0200

    Create Signup API

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,46 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-  res.send("Signup Route");
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 export const login = async (req, res) => {
   res.send("Login Route");
--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,13 @@ import authRoutes from "../backend/routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5999;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use(express.json()); // Allows parsing of incoming requests under req.body
 
 app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port", PORT);
 });
