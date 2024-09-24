9e78ae88302e9c1a1de3b7ca389b9e95c3ba958d
MCDoodle1 <mclarijs@me.com>
2024-09-24 16:35:57 +0200

    Create authentification middleware and API

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "../backend/routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5999;
 
 app.use(express.json()); // Allows parsing of incoming requests under req.body
+app.use(cookieParser()); // Allows parsing of incoming cookies
 
 app.use("/api/auth", authRoutes);
 
