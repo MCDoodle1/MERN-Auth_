f95f0a2b0cc161aba4b9ab2cdca9d110fa47c186
MCDoodle1 <mclarijs@me.com>
2024-09-24 16:11:28 +0200

    Create forgot and reset password API's

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,8 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+} from "./emailTemplates.js";
 import { client, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -21,7 +25,6 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
-
 export const sendWelcomeEmail = async (email, name) => {
   const recipient = [{ email }];
 
@@ -39,5 +42,37 @@ export const sendWelcomeEmail = async (email, name) => {
   } catch (error) {
     console.error("Error sending welcome email", error);
     throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+  try {
+    const response = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+    console.log("Forgot password email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending password reset email", error);
+    throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
+export const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+  try {
+    const response = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Password reset successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+    console.log("Password reset email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending password reset success email", error);
+    throw new Error(`Error sending password reset success email: ${error}`);
   }
 };
