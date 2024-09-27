7703a3b5f96703a7fdb159b8c096510b4a84eb36
MCDoodle1 <mclarijs@me.com>
2024-09-27 15:36:22 +0200

    Created routing and basic CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import VerifyEmailPage from "./pages/VerifyEmailPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage />}></Route>
+      <Route path="/signup" element={<SignUpPage />}></Route>
+      <Route path="/verify-email" element={<VerifyEmailPage />}></Route>
+      <Route path="/forgot-password" element={<ForgotPasswordPage />}></Route>
+      <Route
+        path="/reset-password/:token"
+        element={<ResetPasswordPage />}
+      ></Route>
+    </Routes>
   );
 }
 
